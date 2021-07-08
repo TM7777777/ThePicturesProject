860cc69bef3e03d0184a162f50b0274ea893a6bb
TM7777777 <danbogameden@gmail.com>
2021-07-08 15:33:19 +0300

    added animation titles, added some js code

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ Array.from(button).map((el) => {
     let ind = +e.target.className.replace('pc', '');
     if (document.getElementsByClassName(`pc ${ind}`)[0].style.width === '100px') {
       Array.from(document.querySelectorAll('.pc')).map((el) => (el.style.width = '100px'));
+      Array.from(document.querySelectorAll('.tit')).map((el) => (el.style.opacity = '0'));
       document.getElementsByClassName(`pc ${ind}`)[0].style.width = '850px';
+      document.getElementsByClassName(`tit ${ind}`)[0].style.opacity = '1';
     } else {
       document.getElementsByClassName(`pc ${ind}`)[0].style.width = '100px';
+      document.getElementsByClassName(`tit ${ind}`)[0].style.opacity = '0';
     }
   };
 });
